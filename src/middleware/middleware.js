f4da83eb7fe3f9c55988ae58979dd583e35cb42e
niteshsainicoder <kumarsundratijara@gmail.com>
2024-09-19 21:15:52 +0530

    authentication added

---
+++ src/middleware/middleware.js
@@ -0,0 +1,15 @@
+const apiKeyMiddleware = (req, res, next) => {
+    const api_Key = req.headers['x-api-key']; // Extract API key from headers
+    const validApiKey = process.env.API_KEY; // Access environment variable key
+
+    // Check if the API key matches the stored valid API key
+    if (validApiKey && validApiKey === api_Key) {
+        next(); // Proceed if the API key is valid
+    } else {
+       
+        return res.status(403).json({ success: false, message: 'Invalid API key' });
+    }
+};
+
+export default apiKeyMiddleware;
+
